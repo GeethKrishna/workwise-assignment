1efe7623049d5166e18d127624ca2650b950b91c
GeethKrishna <geethpotnuru211@gmail.com>
2024-12-29 22:55:03 +0530

    changed the nextauth file format

--- src/lib/authHelper.ts
+++ src/lib/authHelper.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "./auth";
 export async function checkauth() {
     const session = await getServerSession(authOptions);
     return session;
